b835a6379470b7df514fdbba5a476875bcabdb1f
thipages <thipages@gmail.com>
2020-01-02 16:12:26 +0100

    typo

--- tests/tests.js
+++ tests/tests.js
@@ -7,12 +7,12 @@ const format=[
     'list',{tag:'ul'}
 ];
 let sources= [
-    `start|A^Here I am^| and |B^Here I stick^|end`,
+    `start|A^Here I am^| and |B^Here I am stuck^|end`,
     `start|A^Here I am|B^embedded^|^|end`,
     `start|list^\nitem1\nitem2\n^|end`,
 ];
 let excepted= [
-    `start<span style="color:red">Here I am</span> and <span style="color:yellow">Here I stick</span>end`,
+    `start<span style="color:red">Here I am</span> and <span style="color:yellow">Here I am stuck</span>end`,
     `start<span style="color:red">Here I am<span style="color:yellow">embedded</span></span>end`,
     `start<ul><li>item1</li><li>item2</li></ul>end`
 ];
