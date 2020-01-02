6c00f67a46b9aa65d090e425962f63f2cbcd6c7d
thipages <thipages@gmail.com>
2020-01-02 16:40:52 +0100

    small changes

--- tests/tests.js
+++ tests/tests.js
@@ -2,19 +2,22 @@ import {text2html} from "../index.js";
 const format=[
     'A',{style:"color:red"},
     'B',{style:"color:yellow"},
-    'Ba',{style:"color:blue"},
-    'A1',{style:"color:orange"},
-    'list',{tag:'ul'}
+    'oops',{style:"color:blue;"},
+    'list',{tag:'ul'},
 ];
 let sources= [
     `start|A^Here I am^| and |B^Here I am stuck^|end`,
     `start|A^Here I am|B^embedded^|^|end`,
     `start|list^\nitem1\nitem2\n^|end`,
+    `I am |oops^stuck^|`,
+    `|list^item1\nitem2^|`
 ];
 let excepted= [
     `start<span style="color:red">Here I am</span> and <span style="color:yellow">Here I am stuck</span>end`,
     `start<span style="color:red">Here I am<span style="color:yellow">embedded</span></span>end`,
-    `start<ul><li>item1</li><li>item2</li></ul>end`
+    `start<ul><li>item1</li><li>item2</li></ul>end`,
+    `I am <span style="color:blue;">stuck</span>`,
+    `<ul><li>item1</li><li>item2</li></ul>`
 ];
 
 sources.forEach(
