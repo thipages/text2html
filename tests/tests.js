068525af613e44b42c35960c9916302191e8e2eb
thipages <thipages@gmail.com>
2020-01-03 17:12:01 +0100

    test delimiters changed

--- tests/tests.js
+++ tests/tests.js
@@ -5,13 +5,15 @@ const format=[
     'oops',{style:"color:blue;"},
     'list',{tag:'ul'},
 ];
-let sources= [
-    `start|A^Here I am^| and |B^Here I am stuck^|end`,
-    `start|A^Here I am|B^embedded^|^|end`,
-    `start|list^\nitem1\nitem2\n^|end`,
-    `I am |oops^stuck^|`,
-    `|list^   item1\nitem2   ^|`
+
+let source= [
+    `start/A_Here I am_/ and /B_Here I am stuck_/end`,
+    `start/A_Here I am/B_embedded_/_/end`,
+    `start/list_\nitem1\nitem2\n_/end`,
+    `I am /oops_stuck_/`,
+    `/list_   item1\nitem2   _/`
 ];
+//let source2= (source,c1,c2)=>source.map(v=>v.replace(/\//g,'|').replace(/_/g,'^'));
 let excepted= [
     `start<span style="color:red">Here I am</span> and <span style="color:yellow">Here I am stuck</span>end`,
     `start<span style="color:red">Here I am<span style="color:yellow">embedded</span></span>end`,
@@ -20,9 +22,9 @@ let excepted= [
     `<ul><li>item1</li><li>item2   </li></ul>`
 ];
 
-sources.forEach(
+source.forEach(
     (v,i)=>{
-        let r=text2html(sources[i],format);
+        let r=text2html(source[i],format,'/','_');
         if (r.output===excepted[i]) {
             console.log("ok");
         } else {
